77cfb341192a82e30c164fd769086136bae43003
flusse333 <73949787+flusse333@users.noreply.github.com>
2021-06-24 03:21:24 +0200

    Add files via upload

---
+++ yup.py
@@ -0,0 +1,12 @@
+fornavn = input ("Hvad er dit fornavn?")
+efternavn = input ("Hvad er dit efternavn?")
+alder = input ("Hvor gammel er du?")
+
+print (fornavn + " " + efternavn + " " + "er " + alder + " år gammel")
+
+if int(alder) >= 18:
+    print ("Du må booze køb en øller")
+else:
+    print ("øv bøv")
+
+chingbong = input ("   ")
